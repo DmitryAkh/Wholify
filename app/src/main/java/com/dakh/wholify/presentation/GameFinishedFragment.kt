784700751c1e2ac4c13a666e558697f77bacaded
DmitryAkh <165409513+DmitryAkh@users.noreply.github.com>
2026-01-03 19:29:42 +0300

    Реализовал GameViewModel

--- app/src/main/java/com/dakh/wholify/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/dakh/wholify/presentation/GameFinishedFragment.kt
@@ -4,15 +4,80 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
-import com.dakh.wholify.R
+import androidx.fragment.app.FragmentManager
+import com.dakh.wholify.databinding.FragmentGameFinishedBinding
+import com.dakh.wholify.domain.entity.GameResult
 
 class GameFinishedFragment : Fragment() {
 
+    private lateinit var result: GameResult
+
+    private var _binding: FragmentGameFinishedBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game_finished, container, false)
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentGameFinishedBinding.inflate(
+            inflater,
+            container,
+            false
+        )
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                retryGame()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner, // удаляет view и callBack. Activity не будет хранить ссылку на объект
+            callback
+        )
+
+        binding.buttonRetry.setOnClickListener {
+            retryGame()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun parseArgs() {
+        result = requireArguments().getParcelable(KEY_GAME_RESULT, GameResult::class.java)
+            ?: throw IllegalArgumentException("Result argument is missing")
+    }
+
+    private fun retryGame() {
+        requireActivity().supportFragmentManager.popBackStack(
+            GameFragment.NAME,
+            FragmentManager.POP_BACK_STACK_INCLUSIVE //удаляет фрагменты до указанного включительно
+        )
+    }
+
+    companion object {
+
+        private const val KEY_GAME_RESULT = "Game result"
+
+        fun getInstance(result: GameResult): GameFinishedFragment {
+            return GameFinishedFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(KEY_GAME_RESULT, result)
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/dakh/wholify/presentation/GameViewModel.kt
@@ -0,0 +1,156 @@
+package com.dakh.wholify.presentation
+
+import android.app.Application
+import android.os.CountDownTimer
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.dakh.wholify.R
+import com.dakh.wholify.data.GameRepositoryImpl
+import com.dakh.wholify.domain.entity.GameResult
+import com.dakh.wholify.domain.entity.GameSettings
+import com.dakh.wholify.domain.entity.Level
+import com.dakh.wholify.domain.entity.Question
+import com.dakh.wholify.domain.useCase.GenerateQuestionUseCase
+import com.dakh.wholify.domain.useCase.GetGameSettingsUseCase
+
+class GameViewModel(application: Application) : AndroidViewModel(application) {
+
+    private lateinit var gameSettings: GameSettings
+    private lateinit var level: Level
+
+    private val context = application
+
+    private val repository = GameRepositoryImpl
+
+    private val generateQuestionUseCase = GenerateQuestionUseCase(repository)
+    private val getGameSettingsUseCase = GetGameSettingsUseCase(repository)
+
+    private var timer: CountDownTimer? = null
+
+    private val _formattedTime = MutableLiveData<String>()
+    val formattedTime: LiveData<String>
+        get() = _formattedTime
+
+    private val _question = MutableLiveData<Question>()
+    val question: LiveData<Question>
+        get() = _question
+
+    private val _percentOfRightAnswers = MutableLiveData<Int>()
+    val percentOfRightQuestion: LiveData<Int>
+        get() = _percentOfRightAnswers
+
+    private val _progressAnswers = MutableLiveData<String>()
+    val progressAnswers: LiveData<String>
+        get() = _progressAnswers
+
+    private val _enoughCount = MutableLiveData<Boolean>()
+    val enoughCount: LiveData<Boolean>
+        get() = _enoughCount
+
+    private val _enoughPercent = MutableLiveData<Boolean>()
+    val enoughPercent: LiveData<Boolean>
+        get() = _enoughPercent
+
+    private val _gameResult = MutableLiveData<GameResult>()
+    val gameResult: LiveData<GameResult>
+        get() = _gameResult
+
+    private val _minPercent = MutableLiveData<Int>()
+    val minPercent: LiveData<Int>
+        get() = _minPercent
+
+    private var countOfRightAnswers = 0
+    private var countOfQuestions = 0
+
+
+    fun startGame(level: Level) {
+        getGameSettings(level)
+        _minPercent.value = gameSettings.minPercentOfRightAnswers
+        startTimer()
+        generateQuestion()
+    }
+
+    fun chooseAnswer(number: Int) {
+        checkAnswer(number)
+        updateProgress()
+        generateQuestion()
+    }
+
+    private fun updateProgress() {
+        val percent = calculatePercentOfRightAnswers()
+        _percentOfRightAnswers.value = percent
+        _progressAnswers.value = String.format(
+            context.resources.getString(R.string.progress_answers),
+            countOfRightAnswers,
+            gameSettings.minCountOfRightAnswers
+        )
+        _enoughCount.value =
+            countOfRightAnswers >= gameSettings.minCountOfRightAnswers
+        _enoughPercent.value = percent >= gameSettings.minPercentOfRightAnswers
+    }
+
+    private fun calculatePercentOfRightAnswers(): Int {
+        return ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
+    }
+
+    private fun checkAnswer(number: Int) {
+        val rightAnswer = question.value?.rightAnswer
+        if (number == rightAnswer) {
+            countOfRightAnswers++
+        }
+        countOfQuestions++
+    }
+
+    private fun getGameSettings(level: Level) {
+        this.level = level
+        gameSettings = getGameSettingsUseCase(level)
+    }
+
+    private fun startTimer() {
+        timer = object : CountDownTimer(
+            gameSettings.gameTimeInSeconds * MILLIS_IN_SECOND,
+            MILLIS_IN_SECOND
+        ) {
+            override fun onFinish() {
+                finishGame()
+            }
+
+            override fun onTick(milliseconds: Long) {
+                _formattedTime.value = formatTime(milliseconds)
+            }
+        }
+        timer?.start()
+    }
+
+    private fun generateQuestion() {
+        _question.value = generateQuestionUseCase(gameSettings.maxSumValue)
+    }
+
+    private fun formatTime(milliseconds: Long): String {
+        val seconds = milliseconds / MILLIS_IN_SECOND
+        val minutes = seconds / SECONDS_IN_MINUTE
+        val leftSeconds = seconds - (minutes * SECONDS_IN_MINUTE)
+        return String.format("%02d:$02d", minutes, leftSeconds)
+    }
+
+    private fun finishGame() {
+        _gameResult.value = GameResult(
+            winner = enoughCount.value == true && enoughPercent.value == true,
+            countOfRightAnswers = countOfRightAnswers,
+            countOfQuestions = countOfQuestions,
+            gameSettings = gameSettings
+        )
+
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer?.cancel()
+    }
+
+    companion object {
+        private const val MILLIS_IN_SECOND = 1000L
+        private const val SECONDS_IN_MINUTE = 60
+    }
+}
