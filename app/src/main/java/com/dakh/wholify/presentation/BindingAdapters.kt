81ac5799cd595881ecd44b7958105bde8dfa4e87
DmitryAkh <165409513+DmitryAkh@users.noreply.github.com>
2026-01-13 19:09:44 +0300

    Добавил Data Binding на GameFinishedFragment.kt

--- app/src/main/java/com/dakh/wholify/presentation/BindingAdapters.kt
+++ app/src/main/java/com/dakh/wholify/presentation/BindingAdapters.kt
@@ -1,16 +1,24 @@
 package com.dakh.wholify.presentation
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.dakh.wholify.R
 import com.dakh.wholify.domain.entity.GameResult
+
+interface OnOptionClickListener{
+    fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.required_score),
-        count
+        count.toString()
     )
 }
 
@@ -28,7 +36,7 @@ fun bindEmojiResult(imageView: ImageView, winner: Boolean) {
 fun bindScoreAnswers(textView: TextView, score: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.score_answers),
-        score
+        score.toString()
     )
 }
 
@@ -36,7 +44,7 @@ fun bindScoreAnswers(textView: TextView, score: Int) {
 fun bindRequiredPercentage(textView: TextView, percentage: Int) {
     textView.text = String.format(
         textView.context.getString(R.string.required_percentage),
-        percentage
+        percentage.toString()
     )
 }
 
@@ -45,9 +53,50 @@ fun bindScorePercentage(textView: TextView, gameResult: GameResult) {
     textView.text =
         String.format(
             textView.context.getString(R.string.score_percentage),
-            getPercentOfRightAnswers(gameResult)
+            getPercentOfRightAnswers(gameResult).toString()
         )
+}
+
+@BindingAdapter("progress")
+fun bindProgressBar(progressBar: ProgressBar, progress: Int) {
+    progressBar.setProgress(progress, true)
+}
+
+@BindingAdapter("answersProgressColor")
+fun bindAnswersProgressColor(textView: TextView, state: Boolean) {
+    textView.setTextColor(getColorByState(textView.context, state))
+}
+
+@BindingAdapter("progressBarColor")
+fun bindProgressBarColor(progressBar: ProgressBar, state: Boolean) {
+    progressBar.progressTintList = ColorStateList.valueOf(
+        getColorByState(
+            progressBar.context,
+            state
+        )
+    )
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+    textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+   textView.setOnClickListener {
+       clickListener.onOptionClick(textView.text.toString().toInt())
+   }
+}
+
+private fun getColorByState(context: Context, state: Boolean): Int {
+    val colorResId = if (state) {
+        android.R.color.holo_green_light
+    } else {
+        android.R.color.holo_red_light
     }
+    return ContextCompat.getColor(context, colorResId)
+}
 
 private fun getPercentOfRightAnswers(gameResult: GameResult) = with(gameResult) {
     ((gameResult.countOfRightAnswers / gameResult.countOfQuestions.toDouble()) * 100).toInt()
--- app/src/main/java/com/dakh/wholify/presentation/GameFragment.kt
+++ app/src/main/java/com/dakh/wholify/presentation/GameFragment.kt
@@ -1,12 +1,9 @@
 package com.dakh.wholify.presentation
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -14,12 +11,9 @@ import androidx.navigation.fragment.navArgs
 import com.dakh.wholify.databinding.FragmentGameBinding
 import com.dakh.wholify.domain.entity.GameResult
 
-
 class GameFragment : Fragment() {
 
     private val args by navArgs<GameFragmentArgs>()
-
-    private lateinit var options: List<Int>
 
     private val viewModelFactory by lazy {
         GameViewModelFactory(
@@ -32,17 +26,6 @@ class GameFragment : Fragment() {
             this,
             viewModelFactory
         )[GameViewModel::class.java]
-    }
-
-    private val tvOptions by lazy {
-        mutableListOf<TextView>().apply {
-            add(binding.tvOption1)
-            add(binding.tvOption2)
-            add(binding.tvOption3)
-            add(binding.tvOption4)
-            add(binding.tvOption5)
-            add(binding.tvOption6)
-        }
     }
 
     private var _binding: FragmentGameBinding? = null
@@ -59,70 +42,29 @@ class GameFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
         observeViewModel()
-        setClickListenersToOptions()
     }
 
     private fun observeViewModel() {
-        viewModel.question.observe(viewLifecycleOwner) {
-            options = it.options
-            with(binding) {
-                tvSum.text = it.sum.toString()
-                tvLeftNumber.text = it.visibleNumber.toString()
-                for (i in 0 until tvOptions.size) {
-                    tvOptions[i].text = it.options[i].toString()
-                }
-            }
-        }
-        viewModel.formattedTime.observe(viewLifecycleOwner) {
-            binding.tvTimer.text = it
-        }
-        viewModel.progressAnswers.observe(viewLifecycleOwner) {
-            binding.tvAnswersProgress.text = it
-        }
-        viewModel.enoughCount.observe(viewLifecycleOwner) {
-            binding.tvAnswersProgress.setTextColor(getColorByState(it))
-        }
-        viewModel.percentOfRightQuestion.observe(viewLifecycleOwner) {
-            binding.progressBar.setProgress(it, true)
-        }
-        viewModel.enoughPercent.observe(viewLifecycleOwner) {
-            val color = getColorByState(it)
-            binding.progressBar.progressTintList = ColorStateList.valueOf(color)
-        }
 
-        viewModel.minPercent.observe(viewLifecycleOwner) {
-            binding.progressBar.secondaryProgress = it
-        }
         viewModel.gameResult.observe(viewLifecycleOwner) {
             launchGameFinishedFragment(it)
         }
     }
-
-    private fun setClickListenersToOptions() {
-        for (i in 0 until tvOptions.size) {
-            tvOptions[i].setOnClickListener { viewModel.chooseAnswer(options[i]) }
-        }
-    }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    private fun getColorByState(state: Boolean): Int {
-        val colorResId = if (state) {
-            android.R.color.holo_green_light
-        } else {
-            android.R.color.holo_red_light
-        }
-        return ContextCompat.getColor(requireContext(), colorResId)
-    }
-
-
     private fun launchGameFinishedFragment(gameResult: GameResult) {
 
-        findNavController().navigate(GameFragmentDirections.actionGameFragmentToGameFinishedFragment(gameResult))
+        findNavController().navigate(
+            GameFragmentDirections.actionGameFragmentToGameFinishedFragment(
+                gameResult
+            )
+        )
     }
 }
